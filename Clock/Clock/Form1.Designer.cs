43432a43b759eb1f38a1656f840b594c5fa81bd0
kim sanghyun <ksh030506@github.com>
2020-06-02 12:13:23 +0900

    위치 조정

--- Clock/Clock/Form1.Designer.cs
+++ Clock/Clock/Form1.Designer.cs
@@ -31,10 +31,10 @@
             this.components = new System.ComponentModel.Container();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.보기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.옵션ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.아날로그ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.디지털ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.종료ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.옵션ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panel1 = new System.Windows.Forms.Panel();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.menuStrip1.SuspendLayout();
@@ -48,7 +48,7 @@
             this.옵션ToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(458, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -62,45 +62,45 @@
             this.보기ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
             this.보기ToolStripMenuItem.Text = "보기";
             // 
-            // 옵션ToolStripMenuItem
-            // 
-            this.옵션ToolStripMenuItem.Name = "옵션ToolStripMenuItem";
-            this.옵션ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
-            this.옵션ToolStripMenuItem.Text = "옵션";
-            // 
             // 아날로그ToolStripMenuItem
             // 
             this.아날로그ToolStripMenuItem.Name = "아날로그ToolStripMenuItem";
-            this.아날로그ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.아날로그ToolStripMenuItem.Size = new System.Drawing.Size(152, 26);
             this.아날로그ToolStripMenuItem.Text = "아날로그";
             this.아날로그ToolStripMenuItem.Click += new System.EventHandler(this.아날로그ToolStripMenuItem_Click);
             // 
             // 디지털ToolStripMenuItem1
             // 
             this.디지털ToolStripMenuItem1.Name = "디지털ToolStripMenuItem1";
-            this.디지털ToolStripMenuItem1.Size = new System.Drawing.Size(224, 26);
+            this.디지털ToolStripMenuItem1.Size = new System.Drawing.Size(152, 26);
             this.디지털ToolStripMenuItem1.Text = "디지털";
             this.디지털ToolStripMenuItem1.Click += new System.EventHandler(this.디지털ToolStripMenuItem1_Click);
             // 
             // 종료ToolStripMenuItem
             // 
             this.종료ToolStripMenuItem.Name = "종료ToolStripMenuItem";
-            this.종료ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.종료ToolStripMenuItem.Size = new System.Drawing.Size(152, 26);
             this.종료ToolStripMenuItem.Text = "종료";
             this.종료ToolStripMenuItem.Click += new System.EventHandler(this.종료ToolStripMenuItem_Click);
             // 
+            // 옵션ToolStripMenuItem
+            // 
+            this.옵션ToolStripMenuItem.Name = "옵션ToolStripMenuItem";
+            this.옵션ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.옵션ToolStripMenuItem.Text = "옵션";
+            // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(53, 79);
+            this.panel1.Location = new System.Drawing.Point(39, 51);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(386, 321);
+            this.panel1.Size = new System.Drawing.Size(375, 369);
             this.panel1.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(458, 450);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
