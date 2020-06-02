982b295534c188af10459baedbdcc1fabf9b18f4
kim sanghyun <ksh030506@github.com>
2020-06-02 10:58:19 +0900

    윈폼으로 아날로그 디지털 시계 프로젝트 만들기

---
+++ Clock/Clock/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.보기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.옵션ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.아날로그ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.디지털ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.종료ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.보기ToolStripMenuItem,
+            this.옵션ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 보기ToolStripMenuItem
+            // 
+            this.보기ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.아날로그ToolStripMenuItem,
+            this.디지털ToolStripMenuItem1,
+            this.종료ToolStripMenuItem});
+            this.보기ToolStripMenuItem.Name = "보기ToolStripMenuItem";
+            this.보기ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.보기ToolStripMenuItem.Text = "보기";
+            // 
+            // 옵션ToolStripMenuItem
+            // 
+            this.옵션ToolStripMenuItem.Name = "옵션ToolStripMenuItem";
+            this.옵션ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.옵션ToolStripMenuItem.Text = "옵션";
+            // 
+            // 아날로그ToolStripMenuItem
+            // 
+            this.아날로그ToolStripMenuItem.Name = "아날로그ToolStripMenuItem";
+            this.아날로그ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.아날로그ToolStripMenuItem.Text = "아날로그";
+            // 
+            // 디지털ToolStripMenuItem1
+            // 
+            this.디지털ToolStripMenuItem1.Name = "디지털ToolStripMenuItem1";
+            this.디지털ToolStripMenuItem1.Size = new System.Drawing.Size(224, 26);
+            this.디지털ToolStripMenuItem1.Text = "디지털";
+            // 
+            // 종료ToolStripMenuItem
+            // 
+            this.종료ToolStripMenuItem.Name = "종료ToolStripMenuItem";
+            this.종료ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.종료ToolStripMenuItem.Text = "종료";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(53, 79);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(386, 321);
+            this.panel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 보기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 아날로그ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 디지털ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 종료ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 옵션ToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Clock/Clock/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class Form1 : Form
+    {
+
+        Graphics g;
+        private bool aClock_Flag = false;
+        private Point Center; //중심점
+        private double radius;//반지름
+        private int hourHand; //시침
+        private int minHand;  //분침
+        private int secHand;  //초침
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
