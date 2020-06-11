ceb3047b79ddf4ddde680717f1943078c54a0292
kim sanghyun <ksh030506@github.com>
2020-06-12 08:59:50 +0900

    Mutex

--- WindowsFormsApp6/WindowsFormsApp6/Program.cs
+++ WindowsFormsApp6/WindowsFormsApp6/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,9 +16,24 @@ namespace WindowsFormsApp6
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
+            bool newForm = false;
+            Mutex mutex = new Mutex(true, Assembly.GetExecutingAssembly().FullName, out newForm);
+            //초기 소유권 가질지, name, 초기 소유권 결과값 할당
+            //1. 어셈블리 이름 -> dll, exe 파일이름
+
+
+            //2. mutex가 생성됬는지 check
+            if(newForm == true)
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+            //3. 하나의 프로그램만 실행 될 수 있도록 변경
+
+            /*Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form1());*/
         }
     }
 }
